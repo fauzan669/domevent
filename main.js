b7541f39a4749252cc86e12a8a80580982f5eeb3
dedi2905 <dedihernandez504@gmail.com>
2020-09-30 08:59:17 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const showModal = () => {
+  let modal = document.getElementById("modalMenu");
+  modal.style.display = "flex";
+};
+
+const closeModal = () => {
+  let modal = document.getElementById("modalMenu");
+  modal.style.display = "none";
+};
